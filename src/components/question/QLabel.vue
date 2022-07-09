df4b16850a358a6413ab9070cbc70de805101a92
RobertM <robert_am@outlook.com>
2022-07-08 19:52:26 -0500

    Add components for questions

---
+++ src/components/question/QLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="text-align: center">
+    <h4>{{ content }}</h4>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    value: String
+  },
+  data() {
+    return {
+      content: this.value
+    }
+  },
+}
+</script>
