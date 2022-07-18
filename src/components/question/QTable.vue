05cffaee1e412f357c98736ed313bbf7a5280ddd
RobertM <robert_am@outlook.com>
2022-07-17 19:07:59 -0500

    New type of field configuration

--- src/components/question/QTable.vue
+++ src/components/question/QTable.vue
@@ -81,7 +81,7 @@
                                   no-title
                                   v-model="$data['editedItem'][field.name]"
                                   @input="field.isEnable = false"
-                                  @change="$data['editedItem'][field.name] = field.dateFormatted = parseDate($data['editedItem'][field.name])"
+                                  @change="field.dateFormatted = parseDate($data['editedItem'][field.name])"
                               ></v-date-picker>
                             </v-menu>
                           </div>
